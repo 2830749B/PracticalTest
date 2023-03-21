36e8383e08c11328e10a961290aa6da9ad027a14
2830749B <2830749B@student.gla.ac.uk>
2023-03-21 19:32:56 +0000

    Practical Test

--- practicaltest/models.py
+++ practicaltest/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Page(models.Model):
     title = models.CharField(max_length=128)
     author = models.CharField(max_length=128)
-    date = models.DateField()
-    description = models.TextField()
+    date = models.CharField(max_length=128)
+    description = models.CharField(max_length=128)
 
 
     def __str__(self):
-        return self.title + self.author + self.date + self.description
+        return self.title
--- practicaltest/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from practicaltest import views
-
-app_name = 'practicaltest'
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    ]
